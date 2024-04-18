7384d79e8f7a6e01cfb3ecfa79df2b1e2dc9e950
Vinit Saini <vinit.saini@ibm.com>
2024-04-19 00:39:08 +0100

    new orgs added for my usecase

---
+++ organization/visaworld/contract/index.js
@@ -0,0 +1,10 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+'use strict';
+
+const visaAppContract = require('./lib/visaappcontract.js');
+module.exports.contracts = [visaAppContract];
